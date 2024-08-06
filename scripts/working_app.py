71780f6527335b0c37d3faac6e724894a6240110
Grant Geist <granthgeist@gmail.com>
2024-08-06 12:22:14 -0500

    Working copy of app pointing at AWS

--- scripts/working_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# Initialize connection.
-conn = st.connection("postgresql", type="sql")
-
-# Perform query.
-df = conn.query('SELECT * FROM national_walkability_index LIMIT 5', ttl="10m")
-
-st.dataframe(df)
